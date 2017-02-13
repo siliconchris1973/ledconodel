791cc91d5c8ed69fd7d887f5459aa9ce6957e84e
siliconchris1973 <chris.guenther@mac.com>
2017-02-13 10:11:09 +0100

    initial blink.js creation - not yet runnable

---
+++ blink.js
@@ -0,0 +1,75 @@
+/*
+ * include the adafruit.io client library to watch the feed
+ */
+require xxx as adaio
+
+/*
+ * the hardware pins for the leds
+ */
+var greenled = 19;
+var redled = 13;
+var blueled = 26;
+
+/*
+ * last state of the leds 
+ * in case we blink, this will stop the blinking
+ * in case the leds are on, this will turn off the leds
+ * in case leds are off and command is blink then blink they will
+ * in case leds are off and command is trigger then on they will go
+ */
+var greenledstatus = false;
+var redledstatus = false;
+var blueledstatus = false;
+
+/*
+ * the name of the Adafruit.io feed we want to watch
+ */
+var feedname = "ledfeeder";
+
+/*
+ * Set the initial state to low.  The state is set prior to the pin becoming
+ * active, so is safe for devices which require a stable setup.
+ */
+rpio.open(greenled, rpio.OUTPUT, rpio.LOW);
+rpio.open(redled, rpio.OUTPUT, rpio.LOW);
+rpio.open(blueled, rpio.OUTPUT, rpio.LOW);
+
+/*
+ * The sleep functions block, but rarely in these simple programs does one care
+ * about that.  Use a setInterval()/setTimeout() loop instead if it matters.
+ */
+function blinkLed(blinkpin; blinkcount) {
+	for (var i = 0; i < blinkcount-1; i++) {
+        	/* On for 1 second */
+        	rpio.write(blinkpin, rpio.HIGH);
+        	rpio.sleep(1);
+		
+	        /* Off for half a second (500ms) */
+	        rpio.write(blinkpin, rpio.LOW);
+	        rpio.msleep(500);
+	}
+}
+
+function watchfeed() { 
+	if i(data == "red") {
+		if (greenon) {
+			
+		} else {
+
+		}
+	} elseif (data == "green") {
+		if (redon) {
+			rpio.open(redled, rpio.OUTPUT, rpio.LOW);	
+		} else {
+
+		}
+	} elseif (data == "blue") {
+		if (blueon) {
+			rpio.open(blueled, rpio.OUTPUT, rpio.LOW);
+		} else {
+
+		}
+	}
+
+}
+
