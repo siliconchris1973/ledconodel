a9b6956d851435e1db682c9a4e1fdc005fa002ea
Christian Günther <chris.guenther@mac.com>
2017-02-13 13:55:31 +0100

    for debugging purposes I commented out all watcher functionality and drove the red led pin manually 1 times

--- blink.js
+++ blink.js
@@ -8,20 +8,13 @@ const name = 'ledconodel';
 debug('booting %s', name);
 
 /*
- *
+ * require rpio2 node module to drive GPIO pins and use hardware pin numbering
  */
-//var rpio = require('rpio2');
 const Gpio = require('rpio2').Gpio;
-
 var options = {
   gpiomem: true,          /* Use /dev/gpiomem */
   mapping: 'physical',    /* Use the P1-P40 numbering scheme */
 }
-
-/*
- * include the adafruit.io client library to watch the feed
- */
-//require xxx as adaio
 
 /*
  * set the hardware pins for the 3 leds as a new instance of Gpio
@@ -33,29 +26,32 @@ const blueled = new Gpio(26);
 /*
  * Set the initial state to low.  The state is set prior to the pin becoming
  * active, so is safe for devices which require a stable setup.
-
-rpio.open(greenled, rpio.OUTPUT, rpio.LOW);
-rpio.open(redled, rpio.OUTPUT, rpio.LOW);
-rpio.open(blueled, rpio.OUTPUT, rpio.LOW);
  */
 greenled.open(Gpio.OUTPUT, Gpio.LOW);
 redled.open(Gpio.OUTPUT, Gpio.LOW);
 blueled.open(Gpio.OUTPUT, Gpio.LOW);
+
+
+/*
+ * include the adafruit.io client library to watch the feed
+ */
+//require xxx as adaio
 
 /*
  * the name of the Adafruit.io feed we want to watch
  */
 var feedname = "ledfeeder";
 
+
+
 /*
  * the main watcher function - need to do this asynchronously
- */
+
 void function watchfeed(data) {
 	if (data == "red") {
     redled.open(Gpio.OUTPUT, Gpio.HIGH);
     redled.sleep(500)
 		redled.open(Gpio.OUTPUT, Gpio.LOW);
-
 	} else if (data == "green") {
     greenled.open(Gpio.OUTPUT, Gpio.HIGH);
     greenled.sleep(500)
@@ -77,6 +73,11 @@ debug('blue blinking');
 watchfeed("blue");
 debug('purple blinking');
 watchfeed("purple");
+*/
+
+redled.open(Gpio.OUTPUT, Gpio.HIGH);
+redled.sleep(500)
+redled.open(Gpio.OUTPUT, Gpio.LOW);
 
 process.on("SIGINT", function(){
   redled.close();
