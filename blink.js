ca21d8ee994c89af8e6878eadc4a1c8c60c5af9d
Christian Günther <chris.guenther@mac.com>
2017-02-13 13:39:04 +0100

    fixed error in instantiating GPIO handling

--- blink.js
+++ blink.js
@@ -10,7 +10,8 @@ debug('booting %s', name);
 /*
  *
  */
-var rpio = require('rpio2');
+//var rpio = require('rpio2');
+const Gpio = require('../lib/index.js').Gpio;
 
 var options = {
   gpiomem: true,          /* Use /dev/gpiomem */
@@ -23,11 +24,24 @@ var options = {
 //require xxx as adaio
 
 /*
- * set the hardware pins for the 3 leds as a new instance of rpio
+ * set the hardware pins for the 3 leds as a new instance of Gpio
  */
-const greenled = new rpio(19);
-const redled = new rpio(13);
-const blueled = new rpio(26);
+const greenled = new Gpio(19);
+const redled = new Gpio(13);
+const blueled = new Gpio(26);
+
+/*
+ * Set the initial state to low.  The state is set prior to the pin becoming
+ * active, so is safe for devices which require a stable setup.
+
+rpio.open(greenled, rpio.OUTPUT, rpio.LOW);
+rpio.open(redled, rpio.OUTPUT, rpio.LOW);
+rpio.open(blueled, rpio.OUTPUT, rpio.LOW);
+ */
+greenled.open(Gpio.OUTPUT, Gpio.LOW);
+redled.open(Gpio.OUTPUT, Gpio.LOW);
+blueled.open(Gpio.OUTPUT, Gpio.LOW);
+
 
 /*
  * last state of the leds
@@ -46,19 +60,6 @@ var blueon = false;
 var feedname = "ledfeeder";
 
 /*
- * Set the initial state to low.  The state is set prior to the pin becoming
- * active, so is safe for devices which require a stable setup.
-
-rpio.open(greenled, rpio.OUTPUT, rpio.LOW);
-rpio.open(redled, rpio.OUTPUT, rpio.LOW);
-rpio.open(blueled, rpio.OUTPUT, rpio.LOW);
- */
-greenled.open(rpio.OUTPUT, rpio.LOW);
-redled.open(rpio.OUTPUT, rpio.LOW);
-blueled.open(rpio.OUTPUT, rpio.LOW);
-
-
-/*
  * The sleep functions block, but rarely in these simple programs does one care
  * about that.  Use a setInterval()/setTimeout() loop instead if it matters.
  */
@@ -75,7 +76,7 @@ void function watchfeed(data) {
 	if (data == "red") {
 		if (redon) {
 			//rpio.open(redled, rpio.OUTPUT, rpio.LOW);
-      redled.open(rpio.OUTPUT, rpio.LOW);
+      redled.open(Gpio.OUTPUT, Gpio.LOW);
 		} else {
 			blinkLed(redled,500);
 		}
@@ -83,7 +84,7 @@ void function watchfeed(data) {
 	} else if (data == "green") {
 		if (greenon) {
 			//rpio.open(greenled, rpio.OUTPUT, rpio.LOW);
-      greenled.open(rpio.OUTPUT, rpio.LOW);
+      greenled.open(Gpio.OUTPUT, Gpio.LOW);
 		} else {
 			blinkLed(greenled,500);
 		}
@@ -91,7 +92,7 @@ void function watchfeed(data) {
 	} else if (data == "blue") {
 		if (blueon) {
 			//rpio.open(blueled, rpio.OUTPUT, rpio.LOW);
-      blueled.open(rpio.OUTPUT, rpio.LOW);
+      blueled.open(Gpio.OUTPUT, Gpio.LOW);
 		} else {
 			blinkLed(blueled,500);
 		}
@@ -109,3 +110,11 @@ debug('blue blinking');
 watchfeed("blue");
 debug('purple blinking');
 watchfeed("purple");
+
+process.on("SIGINT", function(){
+  redled.close();
+  greenled.close();
+  blueled.close();
+  debug('shutdown!');
+  process.exit(0);
+});
