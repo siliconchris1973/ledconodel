dd78710786d706bea44235206bd00136b6d10439
Christian Günther <chris.guenther@mac.com>
2017-02-13 11:27:37 +0100

    included rpio

--- blink.js
+++ blink.js
@@ -1,7 +1,17 @@
 /*
+ * 
+ */
+var rpio = require('rpio');
+
+var options = {
+        gpiomem: true,          /* Use /dev/gpiomem */
+        mapping: 'physical',    /* Use the P1-P40 numbering scheme */
+}
+
+/*
  * include the adafruit.io client library to watch the feed
  */
-require xxx as adaio
+//require xxx as adaio
 
 /*
  * the hardware pins for the leds
@@ -11,15 +21,15 @@ var redled = 13;
 var blueled = 26;
 
 /*
- * last state of the leds 
+ * last state of the leds
  * in case we blink, this will stop the blinking
  * in case the leds are on, this will turn off the leds
  * in case leds are off and command is blink then blink they will
  * in case leds are off and command is trigger then on they will go
  */
-var greenledstatus = false;
-var redledstatus = false;
-var blueledstatus = false;
+var greenon = false;
+var redon = false;
+var blueon = false;
 
 /*
  * the name of the Adafruit.io feed we want to watch
@@ -40,36 +50,38 @@ rpio.open(blueled, rpio.OUTPUT, rpio.LOW);
  */
 function blinkLed(blinkpin; blinkcount) {
 	for (var i = 0; i < blinkcount-1; i++) {
-        	/* On for 1 second */
-        	rpio.write(blinkpin, rpio.HIGH);
-        	rpio.sleep(1);
-		
-	        /* Off for half a second (500ms) */
-	        rpio.write(blinkpin, rpio.LOW);
-	        rpio.msleep(500);
+    /* On for 1 second */
+    rpio.write(blinkpin, rpio.HIGH);
+    rpio.sleep(1);
+
+	  /* Off for half a second (500ms) */
+	  rpio.write(blinkpin, rpio.LOW);
+	  rpio.msleep(500);
 	}
 }
 
-function watchfeed() { 
-	if i(data == "red") {
-		if (greenon) {
-			
-		} else {
-
-		}
-	} elseif (data == "green") {
+function watchfeed() {
+	if (data == "red") {
 		if (redon) {
-			rpio.open(redled, rpio.OUTPUT, rpio.LOW);	
+			rpio.open(redled, rpio.OUTPUT, rpio.LOW);
 		} else {
-
+			blinkLed(redled,5);
 		}
+		redon != redon;
+	} elseif (data == "green") {
+		if (greenon) {
+			rpio.open(greenled, rpio.OUTPUT, rpio.LOW);
+		} else {
+			blinkLed(greenled,5);
+		}
+		greenon != greenon;
 	} elseif (data == "blue") {
 		if (blueon) {
 			rpio.open(blueled, rpio.OUTPUT, rpio.LOW);
 		} else {
-
+			blinkLed(blueled,5);
 		}
+		blueon != blueon;
 	}
 
 }
-
