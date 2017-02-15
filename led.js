134b35b3809ef97ccc466d6ba83818ebf4b048a0
Christian Günther <chris.guenther@mac.com>
2017-02-15 11:00:50 +0100

    added the three leds

--- led.js
+++ led.js
@@ -1,14 +1,25 @@
 var wpi = require('wiring-pi');
 
+/*
+ * set the hardware pins for the 3 leds as a new instance of Gpio
+ */
+const redled = new Gpio(13);
+const greenled = new Gpio(19);
+const blueled = new Gpio(26);
+
 // GPIO pin of the led
-var configPin = 13;
+var configPin = greenled;
 // Blinking interval in usec
 var configTimeout = 1000;
 
 wpi.setup('wpi');
 wpi.pinMode(configPin, wpi.OUTPUT);
 
-var isLedOn = 0;
+var isRedLedOn = 0;
+var isGreenLedOn = 0;
+var isBlueLedOn = 0;
+
+var isLedOn = isGreenLedOn;
 
 setInterval(function() {
 	isLedOn = +!isLedOn;
