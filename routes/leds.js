e7684d53956129116ab4fe7bcdd48825e93682c7
Christian Günther <chris.guenther@mac.com>
2017-02-15 13:56:18 +0100

    fixed typo in leds controller js

--- routes/leds.js
+++ routes/leds.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET LED Controller page. */
-router.get('/leds', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('leds', { title: 'LED Controller' });
 });
 
