7b19684fe3f583d3dd4455154a967fe6390191e3
Christian Günther <chris.guenther@mac.com>
2017-02-15 14:24:34 +0100

    added pictures for leds and test for green led

--- routes/leds.js
+++ routes/leds.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var greenled = require('../public/javascript/greenled.js');
+
 /* GET LED Controller page. */
 router.get('/', function(req, res, next) {
   res.render('leds', { title: 'LED Controller' });
