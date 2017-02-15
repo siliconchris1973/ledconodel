470e59cfe6d984334c6eadc3fc1386077bb779e6
Christian Günther <chris.guenther@mac.com>
2017-02-15 11:01:37 +0100

    changed led setup to hard wired numbering

--- led.js
+++ led.js
@@ -3,9 +3,9 @@ var wpi = require('wiring-pi');
 /*
  * set the hardware pins for the 3 leds as a new instance of Gpio
  */
-const redled = new Gpio(13);
-const greenled = new Gpio(19);
-const blueled = new Gpio(26);
+const redled = 13;
+const greenled = 19;
+const blueled = 26;
 
 // GPIO pin of the led
 var configPin = greenled;
