b9ce9fa01aacd052455401c7b9f2edc6d9e690e4
Christian Günther <chris.guenther@mac.com>
2017-02-15 14:29:02 +0100

    added table for leds

--- routes/leds.js
+++ routes/leds.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var greenled = require('../public/javascript/greenled.js');
-
 /* GET LED Controller page. */
 router.get('/', function(req, res, next) {
   res.render('leds', { title: 'LED Controller' });
