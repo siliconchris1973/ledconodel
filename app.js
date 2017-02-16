2d32454ce6c20c10c6364c80a6fdf0ca478d9019
Christian Günther <chris.guenther@mac.com>
2017-02-16 13:41:33 +0100

    removed users.js - no need for and added leds.pug as entry page

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var leds = require('./routes/leds');
 var redled = require('./routes/driveredled');
 var greenled = require('./routes/drivegreenled');
@@ -27,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/leds', leds);
 app.use('/leds/red', redled);
 app.use('/leds/green', greenled);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('leds', { title: 'LED Controller' });
 });
 
 module.exports = router;
