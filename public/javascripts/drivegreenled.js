f719a81da9ccc7c9e864acf67baead112d955985
Christian Günther <chris.guenther@mac.com>
2017-02-16 14:26:05 +0100

    added configuration from file and put status of led in file

--- public/javascripts/drivegreenled.js
+++ public/javascripts/drivegreenled.js
@@ -8,30 +8,43 @@ const debug = require('debug')('ledconodel');
 const name = 'ledconodel::greenled';
 debug('initiating %s', name);
 
-var wpi = require('wiring-pi');
+var fs = require('fs');
+var configData = fs.readFileSync('../../data/config.json'), myConfigObj;
+
+try {
+	myConfigObj = JSON.parse(configData);
+	console.dir(myConfigObj);
+} catch (err) {
+	console.log('There has been an error parsing your JSON.')
+	console.log(err);
+}
 
 /*
  * set the pins for the 3 leds as a new instance of Gpio
+ * we set these from a config json file
 							HW --> BCM --> WiringPI
 		redled		33 --> 13  --> 23
 		greenled	35 --> 19  --> 24
 		blueled		37 --> 26  --> 25
 */
-const redled = 23;
-const greenled = 24;
-const blueled = 25;
+const redled = myConfigObj.redled;
+const greenled = myConfigObj.greenled;
+const blueled = myConfigObj.blueled;
 
 // Blinking interval in usec
-var configTimeout = 1000;
+var configTimeout = myConfigObj.configtimeout;
+
+// now setup the GPIO
+var wpi = require('wiring-pi');
 
 wpi.setup('wpi');
 wpi.pinMode(redled, wpi.OUTPUT);
 wpi.pinMode(greenled, wpi.OUTPUT);
 wpi.pinMode(blueled, wpi.OUTPUT);
 
-var isRedLedOn = 0;
-var isGreenLedOn = 0;
-var isBlueLedOn = 1;
+var isRedLedOn = fs.readFileSync('../../data/red_status');
+var isGreenLedOn = fs.readFileSync('../../data/green_status');
+var isBlueLedOn = fs.readFileSync('../../data/blue_status');
 var isLedOn = isGreenLedOn;
 var configPin = greenled;
 
@@ -50,21 +63,21 @@ function exitHandler(options, err) {
 /*
  * drive the led
  */
-function turnonled(){
+exports.turnonled function(){
 	 wpi.digitalWrite(configPin,1)
 }
 
 /*
  * drive the led
  */
-function turnoffled(){
+exports.turnoffled function(){
 	 wpi.digitalWrite(configPin,0)
 }
 
 /*
  * blink the given led
  */
-function blinkled(){
+exports.blinkled function(){
 	setInterval(function() {
 		isLedOn = +!isLedOn;
 		//isLedOn = !isLedOn;
