a1429e30f673d005083751fe897b9122306c68d6
Christian Günther <chris.guenther@mac.com>
2017-02-16 13:13:31 +0100

    changed router to app for post action to led driver

--- routes/driveredled.js
+++ routes/driveredled.js
@@ -4,22 +4,22 @@ var led = require('../public/javascripts/driveredled');
 
 /* GET Red LED page. */
 router.get('/', function(req, res, next) {
-  res.render('redled', { title: 'Red LED' });
+  res.render('oneled', { title: 'Red LED' });
 });
 
 
 // base path is already set as being /leds/{color}
-router.post('/on', function(req, res) {
+app.post('/on', function(req, res) {
   led.turnledon();
 });
 
 // base path is already set as being /leds/{color}
-router.post('/off', function(req, res) {
+app.post('/off', function(req, res) {
   led.turnledoff();
 });
 
 // base path is already set as being /leds/{color}
-router.post('/blink', function(req, res) {
+app.post('/blink', function(req, res) {
   led.blinkled();
 });
 
